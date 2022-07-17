bd8aff18f4c13cff696d0e05aaa5c1bfbeb889b4
Loyio <i@loyio.me>
2022-07-17 15:09:01 +0800

    add rasterize homework and fix some bugs

---
+++ Assignment2/Rasterizer/global.h
@@ -0,0 +1,11 @@
+//
+// Created by Loyio Hex on 7/17/22.
+//
+
+#ifndef RASTERIZER_GLOBAL_H
+#define RASTERIZER_GLOBAL_H
+
+//#define MY_PI 3.1415926
+//#define TWO_PI (2.0* MY_PI)
+
+#endif //RASTERIZER_GLOBAL_H
